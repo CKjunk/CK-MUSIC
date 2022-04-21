9e400d743b8765c9178ee8d6c8b6b2fbbbac27fc
ck138130 <15151514819@163.com>
2022-04-21 23:25:10 +0800

    first commit

---
+++ src/main/java/com/cktech/ckmusic/CkMusicApplication.java
@@ -0,0 +1,13 @@
+package com.cktech.ckmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CkMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cktech/ckmusic/CkMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cktech.ckmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
